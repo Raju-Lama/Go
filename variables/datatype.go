60ad344f5528c409db3beee47e43d50a36fcb08c
rajukaji <rajulamarainon@gmail.com>
2025-07-10 22:08:31 +0545

    variables in go:

---
+++ variables/datatype.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var inttype int = 24
+	var stringtype string = "hello, world!"
+
+	fmt.Println("Integer type:", inttype)
+	fmt.Println("String type:", stringtype)
+}
